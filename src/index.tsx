0b86f01ad50cb6ab6e8c9d99962088fe893d30f4
menghonglin <mhlplus@outlook.com>
2021-10-09 18:10:04 +0800

    chore:add typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
-console.log('hello world')
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LanguageSwitch from 'components/LanguageSwitch'
+import LanguageSwitch from './components/LanguageSwitch/index';
+
+console.log('hello world')
+
+
 function App(){
     return (<div><LanguageSwitch/>hello world</div>);
 }
